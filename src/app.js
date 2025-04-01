88ccedcb896955c0aae9aa60444100ec2c2a2434
Bappy38 <iqbal.bappy.13@gmail.com>
2025-04-01 13:15:10 +0600

    DB Connection Established

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const connectDB = require("./config/database");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-    res.send("Hello from the test server");
-})
-
-app.listen(3000, () => {
-    console.log("Server is successfully listening on port 3000...");
-});
+connectDB()
+    .then(() => {
+        console.log("Database connection established");
+        app.listen(3000, () => {
+            console.log("Server is successfully listening on port 3000...");
+        });
+    })
+    .catch((err) => {
+        console.error("Failed to connect database");
+    });
