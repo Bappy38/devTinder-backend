df9c6cd73426850f518af147a44b592d0a04a56e
Bappy38 <iqbal.bappy.13@gmail.com>
2025-04-02 14:05:54 +0600

    UPDATE and DELETE User API Implemented

--- src/app.js
+++ src/app.js
@@ -33,16 +33,31 @@ app.get("/users", async (req, res) => {
 });
 
 app.get("/users/:userId", async (req, res) => {
-    console.log(req.params.userId);
     const user = await User.findById(req.params.userId);
 
     if (!user) {
         res.status(404).send("User not found");
         return;
     }
-    
+
     res.send(user);
-})
+});
+
+app.patch("/users/:userId", async (req, res) => {
+
+    const updateMetadata = await User.findByIdAndUpdate(req.params.userId, {firstName: req.body.firstName, lastName: req.body.lastName}, {includeResultMetadata: true});
+    if (!updateMetadata.value) {
+        res.status(404).send("User not found");
+        return;
+    }
+    res.send("User updated successfully");
+});
+
+app.delete("/users/:userId", async (req, res) => {
+
+    await User.findByIdAndDelete(req.params.userId);
+    res.send("User deleted successfully");
+});
 
 app.use((err, req, res, next) => {
 
